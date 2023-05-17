118e0cd4f82a8c28fb1921d5af0fcad1ba50f18a
Sergei-Gots <Sergei.Gots@yahoo.com>
2023-05-18 02:30:23 +0300

    hw 2-18: Initial commit

---
+++ src/main/java/pro/sky/hogwarts/school/HogwartsSchoolApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/hogwarts/school/SkyproHw218SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hogwarts.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproHw218SchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
