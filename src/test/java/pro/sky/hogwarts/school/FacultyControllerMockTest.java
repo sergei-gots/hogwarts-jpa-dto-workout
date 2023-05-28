cfe78cfe28d8aaaca7c400b26cf2cbaa2cbe93a1
Sergei-Gots <Sergei.Gots@yahoo.com>
2023-05-28 21:35:41 +0300

    hw 3-6 Mock WebMVC-tests for FacultyController. create/get/delete tests completed.

--- src/test/java/pro/sky/hogwarts/school/FacultyControllerMockTest.java
+++ src/test/java/pro/sky/hogwarts/school/FacultyControllerMockTest.java
@@ -21,6 +21,7 @@ import pro.sky.hogwarts.school.repository.StudentRepository;
 import pro.sky.hogwarts.school.service.FacultyService;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -44,17 +45,14 @@ public class FacultyControllerMockTest {
     //Therefore, we will check how the controller and service works,
     //and we will not check what occurs in the database.
 
-    private final String url = "/faculty";
+    private final String url = "/faculty/";
     @Autowired
     private ObjectMapper objectMapper;
     private final Faker faker = new Faker();
 
     @Test
     public void create_test() throws Exception {
-        Faculty faculty = new Faculty();
-        faculty.setId(1L);
-        faculty.setName(faker.harryPotter().house());
-        faculty.setColor(faker.color().name());
+        Faculty faculty = generateFaculty(1L);
 
         when(facultyRepository.save(any())).thenReturn(faculty);
 
@@ -79,5 +77,119 @@ public class FacultyControllerMockTest {
                                     .usingRecursiveComparison().isEqualTo(faculty);
                         }
                 );
+    }
+
+    @Test
+    public void get_test() throws Exception {
+        long id = 1;
+        Faculty faculty = generateFaculty(id);
+        Optional<Faculty> optionalFaculty = Optional.of(faculty);
+
+        when(facultyRepository.findById(any())).thenReturn(optionalFaculty);
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .get(url + id)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(result -> {
+                            MockHttpServletResponse mockHttpServletResponse =
+                                    result.getResponse();
+                            assertThat(mockHttpServletResponse.getStatus())
+                                .isEqualTo(HttpStatus.OK.value());
+                            Faculty facultyResult = objectMapper.readValue(
+                            mockHttpServletResponse.getContentAsString(StandardCharsets.UTF_8),
+                            Faculty.class);
+                            assertThat(facultyResult)
+                                .isNotNull()
+                                .usingRecursiveComparison().isEqualTo(faculty);
+
+                        }
+                );
+    }
+
+    @Test
+    public void get_when_idIsNotListed_test() throws Exception {
+        long id = 1;
+        long wrongId = 2;
+        Faculty faculty = generateFaculty(id);
+
+        when(facultyRepository.findById(any())).thenReturn(Optional.empty());
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .get(url + wrongId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(result -> {
+                            MockHttpServletResponse mockHttpServletResponse =
+                                    result.getResponse();
+                            assertThat(mockHttpServletResponse.getStatus())
+                                .isEqualTo(HttpStatus.NOT_FOUND.value());
+                            String content = mockHttpServletResponse.getContentAsString();
+                            assertThat(content.isEmpty()).isTrue();
+                        }
+                );
+    }
+
+    @Test
+    public void delete_test() throws Exception {
+        long id = 1;
+        Faculty faculty = generateFaculty(id);
+        Optional<Faculty> optionalFaculty = Optional.of(faculty);
+
+        when(facultyRepository.findById(any())).thenReturn(optionalFaculty);
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .delete(url + id)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(result -> {
+                            MockHttpServletResponse mockHttpServletResponse =
+                                    result.getResponse();
+                            assertThat(mockHttpServletResponse.getStatus())
+                                .isEqualTo(HttpStatus.OK.value());
+                            Faculty facultyResult = objectMapper.readValue(
+                            mockHttpServletResponse.getContentAsString(StandardCharsets.UTF_8),
+                            Faculty.class);
+                            assertThat(facultyResult)
+                                .isNotNull()
+                                .usingRecursiveComparison().isEqualTo(faculty);
+
+                        }
+                );
+    }
+
+    @Test
+    public void delete_when_idIsNotListed_test() throws Exception {
+        long id = 1;
+        long wrongId = 2;
+        Faculty faculty = generateFaculty(id);
+
+        when(facultyRepository.findById(any())).thenReturn(Optional.empty());
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .delete(url + wrongId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(result -> {
+                            MockHttpServletResponse mockHttpServletResponse =
+                                    result.getResponse();
+                            assertThat(mockHttpServletResponse.getStatus())
+                                .isEqualTo(HttpStatus.NOT_FOUND.value());
+                            String content = mockHttpServletResponse.getContentAsString();
+                            assertThat(content.isEmpty()).isTrue();
+                        }
+                );
+    }
+
+    private Faculty generateFaculty(long id) {
+        Faculty faculty = new Faculty();
+        faculty.setId(id);
+        faculty.setName(faker.harryPotter().house());
+        faculty.setColor(faker.color().name());
+        return faculty;
     }
 }
