cf655e4ba98248a392d28e9649514c09f251ca38
Sergei Gots <Serge.Gots@gmail.com>
2023-05-30 18:08:21 +0300

    hw 4.2. DB Scheme management comleted

--- scripts421.sql
+++ scripts421.sql
@@ -2,7 +2,7 @@
 ALTER TABLE students ADD CONSTRAINT min_age CHECK ( age >= 16 );
 ALTER TABLE students ALTER COLUMN name SET NOT NULL ;
 ALTER TABLE students ADD CONSTRAINT unique_name UNIQUE(name);
-/* But the next one will be redundant: */
+/* But the next one will be redundant:  */
 ALTER TABLE students ALTER COLUMN age SET DEFAULT 20;
 
 ALTER TABLE faculties ADD UNIQUE (name, color);
