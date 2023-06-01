57723daacf053a26a200255be797b10032d93b76
Sergei-Gots <Sergei.Gots@yahoo.com>
2023-06-01 20:58:16 +0300

    hw 4-1 Tuned

--- src/main/java/pro/sky/hogwarts/school/repository/StudentRepository.java
+++ src/main/java/pro/sky/hogwarts/school/repository/StudentRepository.java
@@ -3,12 +3,10 @@ package pro.sky.hogwarts.school.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import pro.sky.hogwarts.school.entity.Student;
 
 import java.util.Collection;
 
-@Repository
 public interface StudentRepository  extends JpaRepository<Student, Long> {
     Collection<Student> findByAge(int age);
     Collection<Student> findByAgeBetween(int ageMin, int ageMax);
