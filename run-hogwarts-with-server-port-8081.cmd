7167acce3c9b853cab7b28b98c7b6b3453a52d88
Sergei Gots <74458698+Sergei-Gots@users.noreply.github.com>
2023-06-06 01:24:23 +0300

    Update run-hogwarts-with-server-port-8081.cmd

--- run-hogwarts-with-server-port-8081.cmd
+++ run-hogwarts-with-server-port-8081.cmd
@@ -1,6 +1,6 @@
 export JAVA_HOME=/home/serge/.jdks/corretto-11.0.18 -
 
 mvn clean install
-mvn install -DskipTests
+@REM mvn install -DskipTests
 
 java -jar target/hogwarts-school-0.0.1-SNAPSHOT.jar --spring.profiles.active=server-port-8081
